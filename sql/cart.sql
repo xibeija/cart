1f6f12064c127177ef7d22fd8236efb87b1360d2
xibeijia <xibeijia@xibeijia-PC>
2019-06-30 23:57:31 +0800

    sql

---
+++ sql/cart.sql
@@ -0,0 +1,75 @@
+# Host: 127.0.0.1  (Version: 5.5.15)
+# Date: 2019-06-30 23:56:03
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES gb2312 */;
+
+#
+# Structure for table "order_"
+#
+
+DROP TABLE IF EXISTS `order_`;
+CREATE TABLE `order_` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+#
+# Data for table "order_"
+#
+
+INSERT INTO `order_` VALUES (1,1);
+
+#
+# Structure for table "orderitem"
+#
+
+DROP TABLE IF EXISTS `orderitem`;
+CREATE TABLE `orderitem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+#
+# Data for table "orderitem"
+#
+
+INSERT INTO `orderitem` VALUES (1,4,1,1),(2,3,1,1),(3,2,1,1),(4,1,1,1);
+
+#
+# Structure for table "product"
+#
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `price` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "product"
+#
+
+INSERT INTO `product` VALUES (1,'ºÚÉ«µÄË¿Íà',500),(2,'³äÆøÍÞÍÞ',2500),(3,'Æ¤±Þ',180),(4,'À¯Öò',0.2);
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'tom','123');
